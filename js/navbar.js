4497e930571db5238a7aa141149127ef48d12542
balikcina <xlimcs@gmail.com>
2014-04-16 00:02:46 -0700

    UI improvements
    search bar, footer, fonts

--- js/navbar.js
+++ js/navbar.js
@@ -3,3 +3,9 @@ $( document ).ready(function() {
         $("#gen-navbar").load("navbar.html"); 
     });
 });
+
+$( document ).ready(function() {
+    $("#gen-footer").html(function(){
+        $("#gen-footer").load("footer.html"); 
+    });
+});
